8a010d39ead9c3c5aad2e057a2521bf81ca5acd5
huongse7en <huongse7en>
2020-07-24 10:07:11 +0700

    tesT

---
+++ build/precache-manifest.4e5a14a12985df0d6502e59472591a1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9de08c0864726e7c2819728b2870f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afe198bf4a76e797f5c0",
+    "url": "/static/css/main.b7984510.chunk.css"
+  },
+  {
+    "revision": "a2dae637af4cd7e492c7",
+    "url": "/static/js/2.331dfaf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.331dfaf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afe198bf4a76e797f5c0",
+    "url": "/static/js/main.105a3cf1.chunk.js"
+  },
+  {
+    "revision": "172110f1dc1dda15e6cd",
+    "url": "/static/js/runtime-main.db8d1f41.js"
+  },
+  {
+    "revision": "64f755c36dee903bd00e2d7be837d577",
+    "url": "/static/media/icons8-checkmark.64f755c3.svg"
+  }
+]);
